5846d37ba2af2c1a949d3f7b5159ec2c2a1c8732
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-27 17:04:58 +0600

    login page design and functionality done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-document.getElementById("loginBtn").addEventListener("click", function (e){
+document.getElementById("loginBtn").addEventListener("click", function (e) {
     e.preventDefault()
-    console.log(e);
-    console.log("login");
+    const mobileNum = 1626227321;
+    const pinNum = 14106;
+
+    const mobileNumber = document.getElementById("mobileNumber").value
+    const mobileNumberConvert = parseInt(mobileNumber)
+
+    const pinNumber = document.getElementById("pinNumber").value
+    const pinNumberConvert = parseInt(pinNumber)
+
+    if(mobileNumberConvert  === mobileNum && pinNumberConvert === pinNum){
+        alert("login done")
+        window.location.href = "./home.html"
+    }else{
+        alert("incorrect your number or pin")
+    }
 })
