1423ea9a20c248ea1e3221e01466b99a1ad3fce4
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-04-28 20:14:23 +0600

    cashout feature add

--- home.js
+++ home.js
@@ -7,7 +7,7 @@ addMoneyBtn.addEventListener("click", function (e) {
 
     const bank = document.getElementById("bank").value
     const accountNumber = document.getElementById("accountNumber").value
-    if (accountNumber.length < 11 ) {
+    if (accountNumber.length < 11) {
         alert("please enter your valid number")
         return
     }
@@ -18,7 +18,7 @@ addMoneyBtn.addEventListener("click", function (e) {
     }
 
     const addAmount = parseInt(document.getElementById("addAmount").value)
-    if(isNaN(addAmount) || addAmount <= 0){
+    if (isNaN(addAmount) || addAmount <= 0) {
         alert("please enter your amount")
         return
     }
@@ -32,7 +32,60 @@ addMoneyBtn.addEventListener("click", function (e) {
     document.getElementById("addAmount").value = ""
     document.getElementById("pinInput").value = ""
 
+})
+
+// toggoling
 
 
+// main toggoling button
+const addMoneyBox = document.getElementById("addMoneyBox")
+const cashOutBox = document.getElementById("cashOutBox")
+const transferMoneyBox = document.getElementById("transferMoneyBox")
+const getBonusBox = document.getElementById("getBonusBox")
+const payBillBox = document.getElementById("payBillBox")
+const transactionsBox = document.getElementById("transactionsBox")
 
+// form
+const addMoneyForm = document.getElementById("addMoneyForm")
+const cashOutForm = document.getElementById("cashOutForm")
+
+addMoneyBox.addEventListener("click", function () {
+    cashOutForm.classList.add("hidden")
+    addMoneyForm.classList.remove("hidden")
+
+
+})
+cashOutBox.addEventListener("click", function () {
+    addMoneyForm.classList.add("hidden")
+    cashOutForm.classList.remove("hidden")
+
+})
+
+
+// cashout form validation
+const cashOutBtn = document.getElementById("cashOutBtn")
+
+cashOutBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+
+
+    const cashOutAgentNum = document.getElementById("cashOutAgentNum").value
+    if (cashOutAgentNum.length < 11) {
+        alert("please enter valid agent number")
+        return
+    }
+    const cashOutPinNum = parseInt(document.getElementById("cashOutPinNum").value)
+    if (cashOutPinNum !== validPin) {
+        alert("please enter your valid pin")
+        return
+    }
+
+    const cashOutAddAmount = parseInt(document.getElementById("cashOutAddAmount").value)
+    const availableBlnc = parseInt(document.getElementById("availableBlnc").innerText)
+    const afterCashOut = availableBlnc - cashOutAddAmount
+    document.getElementById("availableBlnc").innerText = afterCashOut
+
+    document.getElementById("cashOutAgentNum").value = ""
+    document.getElementById("cashOutPinNum").value = ""
+    document.getElementById("cashOutAddAmount").value = ""
 })
